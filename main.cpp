80fd5581b28fe80b73ba076429bf4e7fce60c759
Xinquan <royzxq@gmail.com>
2015-06-21 22:36:23 -0400

    init

---
+++ main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Algorithm Programming
+//
+//  Created by XinquanZhou on 6/21/15.
+//  Copyright (c) 2015 Xinquan Zhou. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
